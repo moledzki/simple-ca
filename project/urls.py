c5c98d0affddce1f4ec839360b5b3620932ed663
Maciej Olędzki <maciej.oledzki@gmail.com>
2022-07-06 16:52:43 +0200

    added static files when debug is disabled

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.authtoken import views
+from django.views.static import serve 
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -15,9 +17,17 @@ urlpatterns = [
     url(r'^api-token-auth/$', views.obtain_auth_token),
     url(r'^tz_detect/', include('tz_detect.urls')),
     url(r'^', include('core.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^node_modules/(?P<path>.*)$', serve,{'document_root': os.path.join(settings.BASE_DIR, 'node_modules')}),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static('node_modules', document_root=os.path.join(settings.BASE_DIR, 'node_modules'))
+#from django.conf.urls.static import static
+
+#urlpatterns += 
+#urlpatterns += 
+#urlpatterns += 
+
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static('node_modules', document_root=os.path.join(settings.BASE_DIR, 'node_modules'))
