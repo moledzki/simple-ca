070825a94c76517dbd51f3fb1c2e29cfb3c2a8e0
Maciej Olędzki <maciej.oledzki@gmail.com>
2022-07-07 22:38:14 +0200

    added admin user creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+
+function checkVariables {
+    #1 - variable to check
+    if [[ -z "${!1}" ]]
+    then
+        echo "Required variable ${1} is not set"
+    fi
+}
+
 set -e
+
+checkVariables SECRET_KEY ALLOWED_HOST ADMIN_NAME ADMIN_PASSWORD ADMIN_EMAIL
+
 cd /opt/ca
 
 test ! -f /conf/local_settings.py || cp project/local_settings.sample.py /conf/local_settings.py;
@@ -15,5 +27,6 @@ ln -sv /media media
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py ensure_adminuser --username=${ADMIN_NAME} --email=${ADMIN_EMAIL} --password=${ADMIN_PASSWORD}
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
